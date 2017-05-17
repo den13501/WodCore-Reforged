49a7375dd084363348743de13f69b849975a2496
dufernst <dufernst@epicduf.nl>
2017-05-17 18:52:24 +0200

    Revert "Project: Remove unused authserver"
    This reverts commit d449474e063719852b178395d51e77814bf32af9.

--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -34,6 +34,8 @@
 #include "TCSoap.h"
 #include "Timer.h"
 #include "Util.h"
+#include "AuthSocket.h"
+#include "RealmList.h"
 
 #include "BigNumber.h"
 
@@ -45,19 +47,6 @@
 #include "ServiceWin32.h"
 extern int m_ServiceStatus;
 #endif
-
-enum RealmFlags
-{
-    REALM_FLAG_NONE                              = 0x00,
-    REALM_FLAG_INVALID                           = 0x01,
-    REALM_FLAG_OFFLINE                           = 0x02,
-    REALM_FLAG_SPECIFYBUILD                      = 0x04,
-    REALM_FLAG_UNK1                              = 0x08,
-    REALM_FLAG_UNK2                              = 0x10,
-    REALM_FLAG_RECOMMENDED                       = 0x20,
-    REALM_FLAG_NEW                               = 0x40,
-    REALM_FLAG_FULL                              = 0x80
-};
 
 /// Handle worldservers's termination signals
 class WorldServerSignalHandler : public JadeCore::SignalHandler
