52a778ae9a7574d9ff5fccb640270f38a4c49b4d
fcshden <den13501@yahoo.com.tw>
2021-11-20 12:49:34 +0800

    Renew the outdated player promotion event
    Renew the outdated player promotion event.

---
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -0,0 +1,12 @@
+﻿// The name of this function should match:
+// void Add${NameOfDirectory}Scripts()
+/*void AddSolocraftScripts();
+void AddSC_skip_StarterArea();
+void AddSC_start_equipment();
+*/
+void AddSC_promotion_new_players();
+
+void AddCustomScripts()
+{
+	AddSC_promotion_new_players();
+}
--- src/server/scripts/Custom/promo_new_players.cpp
+++ src/server/scripts/Custom/promo_new_players.cpp
@@ -16,25 +16,26 @@ public:
 
     enum Values
     {
-        February2015 = 1422777600,  ///< Timestamp of 1st of February 2015.
-        March232015  = 1427065200,  ///< Timestamp of 23nd of March 2015.
-        March152015  = 1426374000,  ///< Timestamp of 15nd of March 2015.
-        SwiftNetherDrake = 37015,   ///< SpellId.
-        ImperialQuilen   = 85870,   ///< ItemId
-        ApprenticeRidingSkill = 33388
+        Noverber302021 = 1638201600,  ///< Timestamp of 30nd of Noverber 2021.
+        Noverber302049  = 2521814400,  ///< Timestamp of 23nd of Noverber 2049.
+        Noverber152049 = 2520518400,  ///< Timestamp of 15nd of Noverber 2049.
+        SwiftNetherDrake = 37015,   //SpellId. 迅捷虛空幼龍 https://cn.tbc.wowhead.com/spell=37015
+        BlackQirajiBattletank = 26656, //SpellId. 黑色其拉作戰坦克
+        ImperialQuilen   = 85870,   //ItemId
+        ApprenticeRidingSkill = 33388 //初級騎術
     };
 
     void OnLogin(Player* p_Player)
     {
-        if (time(NULL) < Values::February2015 && p_Player->GetTotalPlayedTime() == 0)
+        if (time(NULL) < Values::Noverber302021 && p_Player->GetTotalPlayedTime() <= 3600)
         {
             p_Player->learnSpell(Values::ApprenticeRidingSkill, false);
-            p_Player->learnSpell(Values::SwiftNetherDrake, false);
+            p_Player->learnSpell(Values::BlackQirajiBattletank, false);
         }
 
-        if (time(nullptr) < Values::March232015
-            && p_Player->GetSession()->GetAccountJoinDate() >= Values::March152015
-            && p_Player->GetTotalPlayedTime() == 0)
+        if (time(nullptr) < Values::Noverber302049
+            && p_Player->GetSession()->GetAccountJoinDate() >= Values::Noverber152049
+            && p_Player->GetTotalPlayedTime() <= 3600)
             p_Player->AddItem(Values::ImperialQuilen, 1);
     }
 };
