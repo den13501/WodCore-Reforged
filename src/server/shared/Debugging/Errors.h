2bfc26b6849ba436797aaec9f583e7dee453f737
fcshden <den13501@yahoo.com.tw>
2021-11-24 05:06:21 +0800

    Core/Debugging: Marked assert functions as non returning
    TC commit: https://github.com/TrinityCore/TrinityCore/commit/8e1601e202708ccd483456f5bfa06957470e2d3c

--- src/server/shared/Debugging/Errors.h
+++ src/server/shared/Debugging/Errors.h
@@ -1,13 +1,24 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-//  MILLENIUM-STUDIO
-//  Copyright 2016 Millenium-studio SARL
-//  All Rights Reserved.
-//
-////////////////////////////////////////////////////////////////////////////////
+/*
+* Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
+* Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+* Copyright (C) 2021 WodCore Reforged
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
-#ifndef ERRORS_H
-#define ERRORS_H
+#ifndef TRINITYCORE_ERRORS_H
+#define TRINITYCORE_ERRORS_H
 
 #include "Common.h"
 #include "Log.h"
@@ -21,3 +32,4 @@
 
 #define ASSERT WPAssert
 #endif
+
